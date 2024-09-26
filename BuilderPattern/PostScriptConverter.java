1676f7f3abaa1d68b0d701aaaf7e3c4162f0c502
JhonHCanoB <johnhcano@gmail.com>
2024-09-26 12:56:32 +0000

    BuilderPattern

---
+++ BuilderPattern/PostScriptConverter.java
@@ -0,0 +1,15 @@
+public class PostScriptConverter extends Converter{
+
+    public String makeLine(){
+        return "makeLine PostScript";
+    }
+
+    public String makeParagraph(){
+        return "makeParagraph PostScript";
+    }
+
+    public String makeTable(){
+        return "makeTable PostScript";
+    }
+
+}
