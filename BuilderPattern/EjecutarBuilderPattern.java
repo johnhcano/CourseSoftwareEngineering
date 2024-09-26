6bd27ab2d5e89b3f0c0e061ce3669d3f5bdff031
JhonHCanoB <johnhcano@gmail.com>
2024-09-26 13:27:18 +0000

    BuilderPattern

--- BuilderPattern/EjecutarBuilderPattern.java
+++ BuilderPattern/EjecutarBuilderPattern.java
@@ -1,9 +1,9 @@
 public class EjecutarBuilderPattern {
     public static void main(String[] args) {
         
-        Conveter objC1 = new ASCIIConverter();
-        Conveter objC2 = new PostScriptConverter();
-        Conveter objC3 = new PDFConverter();
+        Converter objC1 = new ASCIIConverter();
+        Converter objC2 = new PostScriptConverter();
+        Converter objC3 = new PDFConverter();
 
         Reader objReader = new Reader("LINE", objC1);
 
