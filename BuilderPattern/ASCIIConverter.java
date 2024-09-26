bc7882af645980f3a9fc9201795a4b655274c8a3
JhonHCanoB <johnhcano@gmail.com>
2024-09-26 12:51:16 +0000

    BuilderPattern

---
+++ BuilderPattern/ASCIIConverter.java
@@ -0,0 +1,15 @@
+public class ASCIIConverter extends Converter{
+
+    public String makeLine(){
+        return "makeLine ASCII";
+    }
+
+    public String makeParagraph(){
+        return "makeParagraph ASCII";
+    }
+
+    public String makeTable(){
+        return "makeTable ASCII";
+    }
+
+}
---
+++ BuilderPattern/Converter.java
@@ -0,0 +1,20 @@
+public class Converter {
+
+    private String document;
+
+    public String makeLine(){
+        return "";
+    }
+
+    public String makeParagraph(){
+        return "";
+    }
+
+    public String makeTable(){
+        return "";
+    }
+    
+    public String getDocument(){
+        return document;
+    }
+}
