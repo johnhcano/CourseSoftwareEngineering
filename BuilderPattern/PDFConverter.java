482b0ea4d0e43382081319977967d6709bf3612f
JhonHCanoB <johnhcano@gmail.com>
2024-09-26 12:59:20 +0000

    BuilderPattern

---
+++ BuilderPattern/PDFConverter.java
@@ -0,0 +1,15 @@
+public class PDFConverter extends Converter{
+
+    public String makeLine(){
+        return "makeLine PDF";
+    }
+
+    public String makeParagraph(){
+        return "makeParagraph PDF";
+    }
+
+    public String makeTable(){
+        return "makeTable PDF";
+    }
+
+}
