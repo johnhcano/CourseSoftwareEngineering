076199fa5b5af8a78ff5af948e7fdb30a80d24a3
JhonHCanoB <johnhcano@gmail.com>
2024-10-04 13:26:06 +0000

    Creación de la clase Operario

---
+++ BuilderPatternTrabajador/Operario.java
@@ -0,0 +1,17 @@
+public class Operario extends Trabajador {
+    
+    private int id;
+    private String nombre;
+    private double horas;
+    private final double VALOR_HORA = 5500.0;
+
+    public Operario(int id, String nombre, double horas){
+        this.id = id;
+        this.nombre = nombre;
+        this.horas = horas;
+    }
+
+    public double pagar(){
+        return horas * VALOR_HORA;
+    }
+}
