d5111e6957ceeeb9bcf64dbf09da11d58d844638
JhonHCanoB <johnhcano@gmail.com>
2024-10-07 14:45:50 +0000

    Creación del nuevo trabajador Analista

--- BuilderPatternTrabajador/Analista.java
+++ BuilderPatternTrabajador/Analista.java
@@ -4,7 +4,7 @@ public class Analista extends Trabajador {
     private double lineasCodigo;
     private final double VALOR_LINEA_JAVA = 1000;
     
-    public Analista(double salarioIntegral, double lineasCodigo) {
+    public Analista(double salarioIntegral, int lineasCodigo) {
         this.salarioIntegral = salarioIntegral;
         this.lineasCodigo = lineasCodigo;
     }
--- BuilderPatternTrabajador/EjecutarTrabajador.java
+++ BuilderPatternTrabajador/EjecutarTrabajador.java
@@ -6,14 +6,16 @@ public class EjecutarTrabajador {
 
     Trabajador objT1 = new Consultor(0.25, 3000000.0);
     Trabajador objT2 = new Operario(192);
-    Trabajador objT3 = new Gerente(12000000.0);
+    Trabajador objT3 = new Gerente(1200000.0);
+    Trabajador objT4 = new Analista(500000.0, 200);
 
-
-    Trabajador[] t = new Trabajador[3];
+    //Creación del arreglo para guardar en cada posición los trabajadores creados anteriormente 
+    Trabajador[] t = new Trabajador[4];
 
     t[0] = objT1;
     t[1] = objT2;
     t[2] = objT3;
+    t[4] = objT4;
 
     Nomina objNomina1 = new Nomina(t);
 
