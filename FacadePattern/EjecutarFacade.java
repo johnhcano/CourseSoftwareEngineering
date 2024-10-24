546a910a871c5336b1ec9422cd291f7714ca9cec
JhonHCanoB <johnhcano@gmail.com>
2024-10-24 13:33:38 +0000

    Facade

--- FacadePattern/EjecutarFacade.java
+++ FacadePattern/EjecutarFacade.java
@@ -1,8 +1,9 @@
 public class EjecutarFacade {
     public static void main(String[] args) {
         
+        Conexion objConexionMySQL; 
+        objConexionMySQL = new MySQL("athena", "12345", "http://localhost", "3306");
 
-        
     }
     
 }
