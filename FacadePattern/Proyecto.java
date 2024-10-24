27bdb6bf8957c52af0fe53e1b87a57386a02056a
JhonHCanoB <johnhcano@gmail.com>
2024-10-24 13:18:52 +0000

    Facade

---
+++ FacadePattern/Proyecto.java
@@ -0,0 +1,14 @@
+public class Proyecto {
+    
+    private String nombre;
+    private int duracion;
+    private Conexion conexion;
+    
+    public Proyecto(String nombre, int duracion, Conexion conexion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.conexion = conexion;
+    }
+
+    
+}
