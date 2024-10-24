102c38e5c0b9742dca3fc548d2acef9d6eebd95a
JhonHCanoB <johnhcano@gmail.com>
2024-10-24 12:53:46 +0000

    Facade

---
+++ FacadePattern/Conexion.java
@@ -0,0 +1,24 @@
+public class Conexion {
+
+    private String user;
+    private String pass;
+    private String cadenaConexion;
+
+    public Conexion(String user, String pass, String cadenaConexion){
+        this.user = user;
+        this.pass = pass;
+        this.cadenaConexion = cadenaConexion;
+    }
+
+    public boolean conectar(String u, String p, String bd){
+        boolean flag = false;
+        if(this.user.equals(u) && this.pass.equals(p)){
+            flag = true;
+        }
+        return flag;
+    }
+
+    public String toString(){
+        return "Conexion[ Usuario:" + user + " Pass:" + pass + " cadenaConexión:" + cadenaConexion;
+    }
+}
